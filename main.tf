d06a442136f5af701bc40221121531fc7c16861b
iamvictorabedi <iamvictorabedi@gmail.com>
2021-07-20 10:57:02 +0300

    new workflow for iamvictorabedi

--- main.tf
+++ main.tf
@@ -86,3 +86,25 @@ module "db" {
   # ... omitted
 }
 
+module "db" {
+  source = "terraform-aws-modules/rds/aws"
+
+  # Disable creation of RDS instance(s)
+  create_db_instance = false
+
+  # Disable creation of option group - provide an option group or default AWS default
+  create_db_option_group = false
+  db_instance_name = iamvictorabedi
+
+  # Disable creation of parameter group - provide a parameter group or default to AWS default
+  create_db_parameter_group = false
+
+  # Disable creation of subnet group - provide a subnet group
+  create_db_subnet_group = false
+
+  # Enable creation of monitoring IAM role
+  create_monitoring_role = true
+
+  # ... omitted
+}
+
